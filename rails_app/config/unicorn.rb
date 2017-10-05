3368cb47ced7b58b59023770c6079e9a2cf3c0d7
ahyataro <gmwnk183@gmail.com>
2017-10-05 09:23:20 +0900

    nginx+unicorn+rails構成ができたところまで

--- rails_app/config/unicorn.rb
+++ rails_app/config/unicorn.rb
@@ -2,8 +2,8 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
-listen "/tmp/unicorn.sock"
-pid "/tmp/unicorn.pid"
+listen File.expand_path('tmp/sockets/unicorn.sock', ENV['RAILS_ROOT'])
+pid    File.expand_path('tmp/unicorn.pid', ENV['RAILS_ROOT'])
 
 stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
 stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
